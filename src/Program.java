426457d613e8b018ab9a74e4cb1ac8c018836c0c
audriusxp <palm@inbox.lt>
2022-07-11 21:50:40 +0300

    brangiausia listo preke

--- src/Program.java
+++ src/Program.java
@@ -10,32 +10,31 @@ public class Program {
     }
 
 
-
     public static void main(String[] args) {
-        Scanner skaitytuvas= new Scanner(System.in);
-        ArrayList<Preke>prekes=nuskaitoPrekesIsFailo("prekes.csv");
+        Scanner skaitytuvas = new Scanner(System.in);
+        ArrayList<Preke> prekes = nuskaitoPrekesIsFailo("prekes.csv");
 
-        System.out.println("kiekPrekiuSandelyje(prekes) = " + kiekPrekiuSandelyje(prekes));
-
+        System.out.println("Prekiu sandelyje = " + kiekPrekiuSandelyje(prekes));
+        System.out.println("Brangiausia preke = " + listoMaximumas(prekes));
 
 
     }
 
-    public static ArrayList<Preke> nuskaitoPrekesIsFailo(String failoPavadinimas){
-        ArrayList<Preke>prekes= new ArrayList<>();
-        try {File failObjektas =new File(failoPavadinimas);
+    public static ArrayList<Preke> nuskaitoPrekesIsFailo(String failoPavadinimas) {
+        ArrayList<Preke> prekes = new ArrayList<>();
+        try {
+            File failObjektas = new File(failoPavadinimas);
             Scanner skaitytuvas = new Scanner(failObjektas);
             skaitytuvas.nextLine();
-            while (skaitytuvas.hasNextLine()){
-                String eilute=skaitytuvas.nextLine();
-                String[] stulpeliai= eilute.split(",");
+            while (skaitytuvas.hasNextLine()) {
+                String eilute = skaitytuvas.nextLine();
+                String[] stulpeliai = eilute.split(",");
                 Preke laikinaPreke = new Preke(Integer.parseInt(stulpeliai[0]), stulpeliai[1], stulpeliai[2], Double.parseDouble(stulpeliai[3]), Integer.parseInt(stulpeliai[4]), stulpeliai[5], stulpeliai[6]);
                 prekes.add(laikinaPreke);
                 System.out.println("laikinaPreke = " + laikinaPreke);
             }
 
-        }
-        catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("Nera tokio failo");
 
@@ -43,23 +42,27 @@ public class Program {
         return prekes;
 
     }
-    public static int kiekPrekiuSandelyje(List<Preke> prekes){
-        int suma=0;
-        for (Preke preke :prekes){
+
+    public static int kiekPrekiuSandelyje(List<Preke> prekes) {
+        int suma = 0;
+        for (Preke preke : prekes) {
             suma += preke.getKiekisSandelyje();
         }
         return suma;
     }
-    public static Preke listoMaximumas(List<Preke> kaina) {
-        Preke laikinasMaximumas = kaina.get(0);
-        for (Preke preke: kaina) {
-            if (kaina> laikinasMaximumas) {
-                laikinasMaximumas=kaina;
+
+    public static double listoMaximumas(List<Preke> prekes) {
+        double laikinasMaximumas = prekes.get(0).getKaina();
+        for (Preke preke : prekes) {
+            if (preke.getKaina() > laikinasMaximumas) {
+                laikinasMaximumas = preke.getKaina();
             }
 
-            }
         }
+
         return laikinasMaximumas;
 
 
+    }
 }
+
